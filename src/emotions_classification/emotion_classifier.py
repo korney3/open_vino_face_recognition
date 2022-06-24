1026d5efafa389c7ae305f278f6b625ecf555a81
Alenicheva Alisa <koren.iz3x@yandex.ru>
2022-06-24 17:02:32 +0300

    Init

--- src/emotions_classification/emotion_classifier.py
+++ src/emotions_classification/emotion_classifier.py
@@ -1,5 +1,13 @@
+import os
+
 from tensorflow.keras.models import load_model
+
 
 class EmotionClassifier:
     def __init__(self):
-        pass
+        script_path = os.path.dirname(os.path.realpath(__file__))
+        model_path = os.path.join(script_path, "../models_weights")
+        self.model = load_model(os.path.join(model_path, 'kaggle_model_emotions.h5'))
+
+emotion_classifier = EmotionClassifier()
+print()
